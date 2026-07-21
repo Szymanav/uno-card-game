da4f85f9282106ecc87b1722d25a54eb5a3b8577
oliviameager <omeager4@gmail.com>
2026-07-21 18:10:05 -0400

    Created two 1-9 cards for each other

--- classes/deck.py
+++ classes/deck.py
@@ -2,7 +2,7 @@ from card import Card
 import random
 
 class Deck:
-    #Stores all cards
+    # Stores all cards
     def __init__(self):
         self.cards = []
         self.create_deck()
@@ -10,6 +10,12 @@ class Deck:
     def create_deck(self):
         card_color = ["Red", "Blue", "Green", "Yellow"]
     
+        # Create Zero Cards
         for color in card_color:
             self.cards.append(Card(color, "0"))
+
+        # Create cards 1-9
+        for value in range(1, 10):
+            
+
 
