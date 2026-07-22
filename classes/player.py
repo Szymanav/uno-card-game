e92b19bcce37c080dc5b56cb19330f5d73211b59
Ava Szymankiewicz <ava.szy37@gmail.com>
2026-07-22 15:44:13 -0400

    Create Player Class (#10)
    * set up the class
    
    * make sure cards can be removed and added to hand

---
+++ classes/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self):
+        self.hand = []
+
+    def add_card(self, card):
+        self.hand.append(card)
+
+    def remove_card(self, card):
+        self.hand.remove(card)
